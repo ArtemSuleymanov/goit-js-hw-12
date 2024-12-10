9db1af8ffb1b35223fc497260f29b94270e9a932
Artem <suleymanov2007artem@gmail.com>
2024-12-10 17:56:44 +0200

    add load more btn

--- src/main.js
+++ src/main.js
@@ -7,32 +7,38 @@ import "izitoast/dist/css/iziToast.min.css";
 
 const form = document.querySelector(`.form`);
 const input = document.querySelector(`.input`);
+const loadMoreBtn = document.querySelector(`.load-more-btn`);
+const gallery = document.querySelector(`.gallery`);
+const loader = document.querySelector(`.loader`);
 
 form.addEventListener(`submit`, onSearch);
 
-function onSearch(event) {
-    event.preventDefault();
 
-    const query = input.value.trim();
-    if (query === "") {
-        iziToast.error({ title: 'Error', message: 'Please enter a search query' });
-    return;
-    }
 
-    clearGallery();
-    showLoader();
 
-    fetchImg(query)
-    .then(data => {
-    if (data.hits.length === 0) {
-        iziToast.info({ title: 'Info', message: 'Sorry, there are no images matching your search query.' });
-        return;
-    }
-    changeGallery(data.hits);
-    })
-    .catch(error => {
-        iziToast.error({ title: 'Error', message: 'Something went wrong. Please try again later.' });
-        console.error(error);
-    })
-    .finally(() => hideLoader());
-}
+// function onSearch(event) {
+//     event.preventDefault();
+
+//     const query = input.value.trim();
+//     if (query === "") {
+//         iziToast.error({ title: 'Error', message: 'Please enter a search query' });
+//     return;
+//     }
+
+//     clearGallery();
+//     showLoader();
+
+//     fetchImg(query)
+//     .then(data => {
+//     if (data.hits.length === 0) {
+//         iziToast.info({ title: 'Info', message: 'Sorry, there are no images matching your search query.' });
+//         return;
+//     }
+//     changeGallery(data.hits);
+//     })
+//     .catch(error => {
+//         iziToast.error({ title: 'Error', message: 'Something went wrong. Please try again later.' });
+//         console.error(error);
+//     })
+//     .finally(() => hideLoader());
+// }
